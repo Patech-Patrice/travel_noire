26754b7a0f711af2dfde1146df97bcabe19cd6fe
Patrice Drayton <patricedrayton@Patrices-MacBook-Pro.local>
2020-02-25 15:24:38 -0500

    added associations between users and locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ApplicationRecord
+    has_many :users
+    has_many :residents, foreign_key: :hometown_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_193706) do
+ActiveRecord::Schema.define(version: 2020_02_25_201413) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
